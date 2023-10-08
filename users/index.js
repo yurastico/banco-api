50a6132e9a77a69d56317aa57552f7ef1a06c440
Yuri Cunha <yurastico@Yuris-Mac-mini.local>
2023-10-08 14:35:31 -0300

    users

---
+++ users/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const helmet = require("helmet")
+const app = express();
+const router = require("./routes/routes")
+const morgan = require("morgan")
+app.use(express.json())
+app.use(cors())
+app.use(morgan("combined"))
+app.use(helmet())
+app.use("/api/user",router)
+
+
+app.listen(4000,()=>console.log("listening in http://localhost:4000"))
+
+
