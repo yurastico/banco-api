e44b1fbf909f9af7599a35c6b649d2af7b72830d
Yuri Cunha <yurastico@Yuris-Mac-mini.local>
2023-10-08 16:36:05 -0300

    adicionando morgan e helmet

--- costumers/index.js
+++ costumers/index.js
@@ -1,14 +1,14 @@
 const express = require("express")
 const cors = require("cors")
-const jwt = require("jsonwebtoken")
-const auth = require("./middleware/jwt-verify")
+const helmet = require("helmet")
 const app = express()
 const router = require("./routes/routes")
+const morgan = require("morgan")
 app.use(express.json())
 app.use('/customer',router)
 app.use(cors())
-
-
+app.use(morgan("combined"))
+app.use(helmet())
 
 
 app.listen(5532, ()=> console.log("listening on http://localhost:5532"))
